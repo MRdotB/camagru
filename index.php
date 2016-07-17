634f18c43730cacfd83ecb5bb7d572ecf45fd9f2
mrdotb <baptiste.chaleil@gmail.com>
2016-07-17 02:09:31 +0200

    Router call test

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
+include 'app/Router.php';
 
+Router::get('/', function() {
+  echo 'home';
+});
+
+Router::post('/', function() {
+  echo 'home';
+});
+
+
+Router::get('/images/:id', function($slug) {
+  echo 'The slug is: ' . $slug;
+});
+
+
+Router::dispatch();
+//Router::debug();
