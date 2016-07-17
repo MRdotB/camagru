b168f704de706217d1639a24058e73627abf6fbd
mrdotb <baptiste.chaleil@gmail.com>
2016-07-17 20:40:09 +0200

    Include the database configuration in the Database trait.

---
+++ app/Database.trait.php
@@ -0,0 +1,15 @@
+<?php
+trait Database {
+	public function connect() {
+		include 'config/database.php';
+
+		try {
+			$dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $dbh;
+		} catch (PDOException $e) {
+			echo 'Connection failed: ' . $e->getMessage();
+			die();
+		}
+	}
+}
