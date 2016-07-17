b7a347685fa136f553c8d524893757f58045e55d
mrdotb <baptiste.chaleil@gmail.com>
2016-07-17 17:40:38 +0200

    Router almost done

---
+++ app/Router.class.php
@@ -0,0 +1,33 @@
+<?php
+
+include 'RouterUtility.trait.php';
+
+class Router {
+	use RouterUtility;
+
+	public static $routes = [];
+
+	public static function __callstatic($method, $params) {
+		$route = [
+			'uri' => $params[0],
+			'method' => strtoupper($method),
+			'callback' => $params[1]
+		];
+		array_push(self::$routes, $route);
+	}
+
+
+	public static function dispatch(){
+		$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+		$method = $_SERVER['REQUEST_METHOD'];
+
+		$action = RouterUtility::action(self::$routes, $uri, $method);
+		var_dump($action);
+		if ($action['callback'] !== null) {
+			call_user_func_array($action['callback'], $action['args']);
+		} else {
+			// todo
+			echo 404;
+		}
+	}
+}
---
+++ app/RouterUtility.trait.php
@@ -0,0 +1,44 @@
+<?php
+trait RouterUtility {
+	private static function array_cmp_skip($uri_ref, $uri_arg, $index) {
+		if (count($uri_ref) !== count($uri_arg)) {
+			return false;
+		}
+		for ($i = 1; $i < count($uri_ref); $i++) {
+			if ($uri_ref[$i] !== $uri_arg[$i] && $i !== $index) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static function action($routes, $uri, $method) {
+		$action = [
+			'callback' => null,
+			'args' => []
+		];
+		// check simple routes
+		foreach ($routes as $route) {
+			if ($route['method'] === $method && $route['uri'] === $uri) {
+				$action['callback'] = $route['callback'];
+				return $action;
+			}
+		}
+		// regex routes
+		$uri_arg = explode('/', $uri);
+		foreach ($routes as $route) {
+			if ($route['method'] === $method) {
+				$uri_ref = explode('/', $route['uri']);
+				if ($index = array_search(':id', $uri_ref)) {
+					if (self::array_cmp_skip($uri_ref, $uri_arg, $index)) {
+						if (preg_match('/[0-9]+/', $uri_arg[$index])) {
+							$action['callback'] = $route['callback'];
+							$action['args'][] = $uri_arg[$index];
+							return $action;
+						}
+					}
+				}
+			}
+		}
+	}
+}
