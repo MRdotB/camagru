8a3129699f6d8d8c86ba64b43c6548ac4103b782
Baptiste CHALEIL <bchaleil@e3r13p13.42.fr>
2016-07-18 16:39:28 +0200

    Remove debug var_dump

--- app/Router.class.php
+++ app/Router.class.php
@@ -22,7 +22,6 @@ class Router {
 		$method = $_SERVER['REQUEST_METHOD'];
 
 		$action = RouterUtility::action(self::$routes, $uri, $method);
-		var_dump($action);
 		if ($action['callback'] !== null) {
 			call_user_func_array($action['callback'], $action['args']);
 		} else {
