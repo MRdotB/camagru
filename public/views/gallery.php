bf2bb73058ca7097eb391eaa08872e7d63b42fad
Baptiste CHALEIL <bchaleil@e2r10p13.42.fr>
2016-07-20 19:08:42 +0200

    pagination done

--- public/views/gallery.php
+++ public/views/gallery.php
@@ -17,16 +17,31 @@
 		</header>
 		<div id='main'>
 			<article id="article">
-				<div class="gallery--block">
+				<div id="collection" class="gallery--block">
 				<?php 
+					$i = 0;
 					foreach($images as $image) {
-						echo '<div class="gallery--elem"><img data-id="' . $image['id'].'" src="' . $image['path'] . '"/></div>';
+						if (++$i > 3) {
+							echo '<div class="gallery--modifier gallery--elem"><img data-id="' . $image['id'].'" src="' . $image['path'] . '"/></div>';
+						} else {
+							echo '<div class="gallery--elem"><img data-id="' . $image['id'].'" src="' . $image['path'] . '"/></div>';
+						}
 					}
+							?>
+				</div>
+				<?php>
+					$pagination = ceil(count($images) / 3);
+					echo '<ul id="pagination"><li class="disabled"><a><i class="material-icons">chevron_left</i></a></li>';
+					for($i = 0; $i < $pagination; $i++) {
+						echo '<li><a>' . $i . '</a></li>';
+					}
+					echo '<li class="disabled"><a><i class="material-icons">chevron_right</i></a></li></ul>';
 				?>
+				<div>
 				</div>
 			</article>
 		</div>
 		 <footer>footer</footer>
-		<script src="/public/js/main.js"></script>
+		<script src="/public/js/gallery.js"></script>
 	</body>
 </html>
