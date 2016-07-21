8ef020d5845775566cdf9797cb3f2f69c1c9ebc1
Baptiste CHALEIL <bchaleil@e3r2p22.42.fr>
2016-07-22 01:48:29 +0200

    Update route

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 include 'app/Router.class.php';
 include 'app/User.class.php';
 include 'app/Image.class.php';
+include 'app/Comment.class.php';
+include 'app/Like.class.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 session_start();
 // Views
@@ -14,13 +19,40 @@ Router::get('/', function() {
 	include('public/views/home.php');
 });
 Router::get('/gallery', function() {
-	if (!isset($_SESSION['username'])) {
-		header('Location: http://localhost:8080/login');
-	}
 	$image = new Image();
 	$images = $image->get_images();
 	include('public/views/gallery.php');
 });
+Router::get('/gallery/:id', function($id) {
+	$image = new Image();
+	$img = $image->get_image($id);
+	if (!$img) {
+		echo '404';
+		return;
+	}
+	$comment = new Comment();
+	$comments = $comment->get_comment($id);
+	include('public/views/single.php');
+});
+Router::post('/like/:id', function($id) {
+	if (!isset($_SESSION['username'])) {
+		header('Location: http://localhost:8080/login');
+	}
+	$like = new Like();
+	echo $like->like($id);
+});
+Router::post('/unlike/:id', function($id) {
+	if (!isset($_SESSION['username'])) {
+		header('Location: http://localhost:8080/login');
+	}
+	$like = new Like();
+	echo $like->unlike($id);
+});
+Router::post('/likecount/:id', function($id) {
+	$like = new Like();
+	echo $like->like_count($id);
+});
+
 Router::get('/login', function() {
 	if (isset($_SESSION['username'])) {
 		header('Location: http://localhost:8080');
@@ -39,7 +71,23 @@ Router::get('/forgotten', function() {
 	}
 	include('public/views/forgotten.php');
 });
-
+//Comments
+Router::post('/comment/add', function() {
+	if (!isset($_SESSION['username'])) {
+		return false;
+	}
+	$data = [
+		'title' => $_POST['title'],
+		'text' => $_POST['text'],
+		'image_id' => $_POST['image_id']
+	];
+	$comment = new Comment($data);
+	if ($name = $comment->insert_comment($data)) {
+		echo $name;
+	} else {
+		echo 'false';
+	}
+});
 //Users
 Router::post('/user/register', function() {
 	$data = [
@@ -106,6 +154,9 @@ Router::get('/user/logout', function() {
 
 //Images
 Router::post('/image/upload', function() {
+	if (!isset($_SESSION['username'])) {
+		echo json_encode(['error' => 'true']);
+	}
 	$data = [
 		'sunglass' => $_POST['sunglass'],
 		'join' => $_POST['join']
@@ -119,16 +170,16 @@ Router::post('/image/upload', function() {
 	}
 });
 Router::post('/image/delete/:id', function($id) {
+	if (!isset($_SESSION['username'])) {
+		echo 'false';
+	}
 	$image = new Image();
 	header('Content-Type: application/json');
 	if ($result = $image->delete_image($id)) {
 		echo 'true';
-		//echo json_encode(['' => 'message d\'erreur lol.']);
 	} else {
 		echo 'false';
 	}
 });
 
-
-//Router::debug();
 Router::dispatch();
